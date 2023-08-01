d6a19870dec2e37a6292097edb715950932eee6a
kh1012 <kh1012@midasit.com>
2023-08-01 18:07:39 +0900

    @package-version 0.0.6 (export 샘플용)

---
+++ src/lib/button/Styled.tsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 200px;
+	height: 120px;
+	background-color: #ff2c7d;
+	color: white;
+`;
