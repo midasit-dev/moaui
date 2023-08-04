1dfc0ae45c41225317446ee1a8d6e161966b7bab
kh1012 <kh1012@midasit.com>
2023-08-04 15:17:25 +0900

    App.tsx 기본형 추가, babel warning 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,8 @@
 import React from 'react';
-import './App.css';
-import Button from "./lib/button/index";
-import IconButton from "./lib/IconButton/index";
-import QuizIcon from '@mui/icons-material/Quiz';
 function App() {
-  function hanlderButtonClick(){
-    console.log("Button click test");
-  }
-
-  function hanlderIconButtonClick(){
-    console.log("Icon Button click test");
-  }
-  return (
-    <div className="App">
-				<Button onClick={hanlderButtonClick}>
-          Button Text
-        </Button>
-        <br/>
-        <IconButton onClick={hanlderIconButtonClick}>
-          <QuizIcon/>IconButton Text
-        </IconButton>
-    </div>
-  );
+  return ( 
+		<></>
+	);
 }
 
 export default App;
