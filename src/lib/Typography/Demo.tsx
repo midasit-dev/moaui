d441d130dc4b6770b35fd02a86a47b200d68df1d
kh1012 <kh1012@midasit.com>
2023-08-04 15:18:04 +0900

    Panel Component 개발 Demo, test 코드 추가

---
+++ src/lib/Typography/Demo.tsx
@@ -0,0 +1,14 @@
+import Typography from './index';
+
+function Demo() {
+	return (
+		<>
+			<Typography variant='h1' color='primary'>h1 테스트!</Typography>
+			<Typography variant='body1' color='secondary'>body1</Typography>
+			<Typography variant='body2' color='third'>body2</Typography>
+			<Typography variant='body3' color='disable'>body3</Typography>
+		</>
+	);
+}
+
+export default Demo;
