f01d72eb1208d9415ad2c6c6af814ba160963545
kh1012 <kh1012@midasit.com>
2023-08-04 13:15:23 +0900

    dist 파일 삭제

--- dist/button/Styled.js
+++
@@ -1,7 +0,0 @@
-var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
-    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
-    return cooked;
-};
-import styled from "@emotion/styled";
-export var Container = styled.div(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n\tdisplay: flex;\n\tjustify-content: center;\n\talign-items: center;\n\twidth: 200px;\n\theight: 120px;\n\tbackground-color: #ff2c7d;\n\tcolor: white;\n"], ["\n\tdisplay: flex;\n\tjustify-content: center;\n\talign-items: center;\n\twidth: 200px;\n\theight: 120px;\n\tbackground-color: #ff2c7d;\n\tcolor: white;\n"])));
-var templateObject_1;
--- dist/button/index.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import * as Styled from "@moaui/lib/button/Styled";
-function Button() {
-    return _jsx(Styled.Container, { children: "\uBC84\uD2BC \uD14C\uC2A4\uD2B8" });
-}
-export default Button;
