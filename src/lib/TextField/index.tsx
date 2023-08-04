c57823c491995933ebfac89a795e4fa3b7c6ecf1
lhy0118 <lhy0118@midasit.com>
2023-08-04 17:51:44 +0900

    TextField와 같이 사용하는 title 추가 (title 위치 3가지 옵션)
    titleposition : left, label, right

--- src/lib/TextField/index.tsx
+++ src/lib/TextField/index.tsx
@@ -1,25 +1,51 @@
 import React from "react";
-import MoaTextField from "./Styled";
+import MoaTextField, {type  MoaTextFieldProps} from "./Styled";
 import { Typography, Box } from "@mui/material";
 
-type MoaTextfieldProps = {
-	frontText:string,
-	children: string
-}
-
 MoaTextfield.defaultProps = {
-	frontText :"",
-	children: "TextField"
+	title :"",
+	titlePosition: "",
+	defaultValue: "TextField"
 }
 
-function MoaTextfield(props: MoaTextfieldProps) : React.ReactElement {
-	const frontText = props.frontText;
-	const textFieldText:string = props.children;
+function MoaTextfield(props: MoaTextFieldProps) : React.ReactElement {
+	const textFieldText:string = props.defaultValue;
+	const title:string = props.title ? props.title : "";
+	const titlePosition:string = props.titlePosition ? props.titlePosition : "";
+
+	const boxStyle = (position: string) => {
+		if(position === "left")
+			return {
+				display: "inline-flex", alignItems:"center", gap:"0.25rem"
+			}
+		if(position === "label")
+			return {
+				display: "inline-flex",  alignItems:"flex-start", justifyContent:"center"
+			}
+		if(position === "right")
+			return {
+				display: "inline-flex",  alignItems:"center", gap:"0.25rem"
+			}
+	}
+
 	return (
 		<React.Fragment>
-			<Box>
-				<MoaTextField value={textFieldText} />
-			</Box>
+			{ title !== "" &&
+				<Box style={boxStyle(titlePosition)} sx={(titlePosition === "label") ? {flexDirection:"column"} : {flexDirection:"row"}}>
+					{
+						titlePosition === "right" ?
+						<React.Fragment>
+							<MoaTextField defaultValue={textFieldText} />
+							<div>{props.title}</div>
+						</React.Fragment>
+						:
+						<React.Fragment>
+							<div>{props.title}</div>
+							<MoaTextField defaultValue={textFieldText} />
+						</React.Fragment>
+					}
+				</Box>
+			}
 		</React.Fragment>
 	)
 }
