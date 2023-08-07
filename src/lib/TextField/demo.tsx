96c90ac229ff2ed4c30f6b40d07b06c8e42a3236
lhy0118 <lhy0118@midasit.com>
2023-08-07 17:18:25 +0900

    조건문 예외처리 누락 추가

--- src/lib/TextField/demo.tsx
+++ src/lib/TextField/demo.tsx
@@ -5,8 +5,11 @@ function Demo() {
 	return (
 		<React.Fragment>
 			<TextField defaultValue={'TextField Demo'} />
+			<br/><br/>
 			<TextField defaultValue={'TextField left title'} title="left" titlePosition="left"/>
+			<br/><br/>
 			<TextField defaultValue={'TextField label title'} title="label" titlePosition="label"/>
+			<br/><br/>
 			<TextField defaultValue={'TextField right title'} title="right" titlePosition="right"/>
 		</React.Fragment>
 	);
