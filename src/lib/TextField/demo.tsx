786746a83532c871b73c592d7f954913e80999c4
lhy0118 <lhy0118@midasit.com>
2023-08-09 18:59:47 +0900

    demo sample 문구 수정

--- src/lib/TextField/demo.tsx
+++ src/lib/TextField/demo.tsx
@@ -6,9 +6,9 @@ function Demo() {
 		<React.Fragment>
 			<TextField defaultValue={'TextField Demo'} />
 			<br/><br/>
-			<TextField defaultValue={'TextField left title'} title="left" titlePosition="left" error={true}/>
+			<TextField defaultValue={'error left title'} title="left" titlePosition="left" error={true}/>
 			<br/><br/>
-			<TextField defaultValue={'TextField label title'} title="label" titlePosition="label" disabled={true}/>
+			<TextField defaultValue={'disbled label title'} title="label" titlePosition="label" disabled={true}/>
 			<br/><br/>
 			<TextField defaultValue={'TextField right title'} title="right" titlePosition="right"/>
 		</React.Fragment>
