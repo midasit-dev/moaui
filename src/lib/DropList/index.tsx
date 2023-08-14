729a79ebe651f3b08ebd941bdcdc6b754e72a388
lhy0118 <lhy0118@midasit.com>
2023-08-14 18:00:32 +0900

    함수 TSDoc 추가 하여 Props 정보 표시하도록 수정

--- src/lib/DropList/index.tsx
+++ src/lib/DropList/index.tsx
@@ -8,6 +8,11 @@ MoaDroplist.defaultProps = {
 	defaultValue: ""
 }
 
+/**
+ * @param {MoaDropListProps} props - defaultValue, value, itemList, onChange, width
+ * @returns {React.ReactElement} MoaDropList
+ */
+
 export default function MoaDroplist(props: MoaDropListProps) : React.ReactElement {
 	return (
 		<MoaDropList {...props} />
