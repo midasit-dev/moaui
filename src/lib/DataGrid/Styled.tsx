143bbd8a904aa724d7ae009312205d79d5bca09a
bce0404 <bce0404@midasit.com>
2023-08-22 16:11:08 +0900

    DataGrid Style 값 적용

--- src/lib/DataGrid/Styled.tsx
+++ src/lib/DataGrid/Styled.tsx
@@ -1,6 +1,7 @@
 import { styled } from '@mui/material/styles';
 import { DataGrid, DataGridProps } from "@mui/x-data-grid";
 import Color from '../Color';
+import MoaCheck from '../Check';
 
 export interface StyledProps extends DataGridProps {
 	sx?: never,
@@ -10,20 +11,63 @@ type InnerStyledProps = {
 	theme: any;
 };
 
+type Typo = {
+	fontFamily: string,
+	fontSize: string | number,
+}
+
+const typoSet : Typo = {
+	fontFamily: "Pretendard",
+	fontSize: "0.75rem",
+};
+
 const StyledComponent = styled((props: StyledProps) : React.ReactElement => {	
 	const { sx, ...rest } = props;
+	
 	return (
 		<DataGrid
+			checkboxSelection
 			density="compact"
 			{...rest}
-			// slotProps={{
-			// 	cell: {
-			// 		align: "center",
-			// 	},
-			// }}
+			disableColumnFilter
+			disableColumnMenu
+			slots={{
+				baseCheckbox: MoaCheck
+			}}
+			showColumnVerticalBorder
+			slotProps={{
+				row: {
+					rowHeight: 32,
+				},
+				cell: {
+					align: "center",
+					height: 32,
+				},
+			}}
 			sx={{
 				".MuiDataGrid-columnHeaders": {
-					backgroundColor: Color.component.gray_01,
+					...typoSet,
+					fontWeight: 500,
+					backgroundColor: Color.component.gray_light,
+				},
+				".MuiDataGrid-columnSeperator": {
+					color: Color.component.gray,
+				},
+				".MuiDataGrid-columnHeaderTitleContainer": {
+					justifyContent: "center",
+				},
+				".MuiDataGrid-cellContent": {
+					...typoSet,	
+					fontWeight: 400,
+				},
+				".MuiDataGrid-cell:focus": {
+					outline: `solid ${Color.primary.focus} 1px`,
+				},
+				".MuiDataGrid-row.Mui-selected:hover": {
+					backgroundColor: Color.component.gray_02,
+				},
+				".MuiDataGrid-row.Mui-selected": {
+					backgroundColor: Color.component.gray_02,
 				}
 			}}
 		/>
