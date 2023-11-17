922e5a18fd44fd1bbc9ecc45ef1f4f65997b61e3
kh1012 <kh1012@midasit.com>
2023-11-17 10:49:38 +0900

    [StoryBook] Document 대표이름 변경, TOC 적용

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,13 +2,25 @@ import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/i,
+        date: /Date$/,
       },
     },
+		docs: {
+			toc: {
+				contentsSelector: '.sbdocs-content',
+        headingSelector: 'h1, h2, h3',
+        ignoreSelector: '#primary',
+        title: 'Contents',
+        disable: false,
+        unsafeTocbotOptions: {
+          orderedList: true,
+				},
+			},
+		}
   },
 };
 
