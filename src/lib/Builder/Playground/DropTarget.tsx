2e7462137aaf310dfd5c5eec273f417f986e0313
lhy0118 <lhy0118@midasit.com>
2023-11-29 17:53:34 +0900

    Drop 영역을 Row, Column 개수에 따라 동적으로 grid 컴포넌트를 생성하고 Drop 이벤트를 각각 받을 수 있도록 Code Refactoring.
    기존 코드는 Drop 영역을 추가할때마다 컴포넌트, Drop이벤트 코드 등이 재활용 되지 못한 상태.
    -> 동일한 형태의 컴포넌트 코드를 동적으로 생성할 수 있게 수정
    -> Drop 이벤트 코드를 컴포넌트 동적 생성시 각각에 연결시켜주어 코드 재활용성을 높이는 방향으로 수정
    -> Drop 영역에 가져온 컴포넌트의 타입을 관리하는 Hook을 이중배열로 변경하여 하나의 hook 안에서 모든 컴포넌트 타입 관리

--- src/lib/Builder/Playground/DropTarget.tsx
+++ src/lib/Builder/Playground/DropTarget.tsx
@@ -37,9 +37,9 @@ const fontStyle = {
 }
 
 function DraggedComponent(props: any) : any {
-  const itemType = props.itemType;
+  const item = props.item;
 
-  if(itemType.type === ItemTypes.BUTTON){
+  if(item.type === ItemTypes.BUTTON){
     return(
       <MoaButton
         variant={"contained"}
@@ -51,7 +51,7 @@ function DraggedComponent(props: any) : any {
       </MoaButton>
     )
   }
-  else if(itemType.type === ItemTypes.TEXTFIELD){
+  else if(item.type === ItemTypes.TEXTFIELD){
     return (
       <MoaTextField
         width={"auto"}
@@ -69,155 +69,120 @@ function DraggedComponent(props: any) : any {
 function getComponentType(droppedItems: any){
   let ComponentType: string[] = [];
   for(let i=0; i<droppedItems.length; i++){
-    if(droppedItems[i]["props"]["itemType"]["type"] === ItemTypes.BUTTON){
+    if(droppedItems[i]["props"]["item"]["type"] === ItemTypes.BUTTON){
       ComponentType.push(ItemTypes.BUTTON);
-    } else if(droppedItems[i]["props"]["itemType"]["type"] === ItemTypes.TEXTFIELD){
+    } else if(droppedItems[i]["props"]["item"]["type"] === ItemTypes.TEXTFIELD){
       ComponentType.push(ItemTypes.TEXTFIELD);
     }
   }
   return ComponentType;
 }
 
+const DropArea = ({ index, dropped, setDropped, columnCount, rowCount } : any) => {
+  const [{ canDrop, isOver }, drop] = useDrop({
+    accept: [...DropAcceptList],
+    drop: (item, monitor) => {
+        if (monitor.isOver()) {
+          if(dropped.length !== 0){
+            const Compo = [...dropped[index]];
+            if (Compo.length === 2) {
+              alert('해당 영역에 더이상 컴포넌트를 추가할 수 없습니다.');
+            } else {
+              Compo.push(<DraggedComponent item={item} />);
+            }
+            setDropped((prev: any) => {
+              const newDropped = [...prev];
+              newDropped[index] = Compo;
+              return newDropped;
+            });
+          } else {
+            const Compo = [...dropped];
+            Compo.push(<DraggedComponent item={item} />);
+            setDropped((prev: any) => {
+              const newDropped = [...prev];
+              newDropped[index] = Compo;
+              return newDropped;
+            });
+          }
+      }
+    },
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
+  });
+
+  const isActive = canDrop && isOver;
+
+  return (
+    <Grid item xs={12 / columnCount} style={{height: `${Math.floor(100 / rowCount)}%`}}>
+      <div
+        ref={drop}
+        style={{
+          width: '100%',
+          height: '100%',
+          border: dropped.length === 0 ?
+                    (isActive ? '1px solid gray' : '1px dashed gray' )
+                  :
+                    (dropped[index].length === 0 ? (isActive ? '1px dashed gray' : '1px solid gray') : "0"),
+          backgroundColor: isActive ? '#EEE' : 'transparent',
+          margin: "0.1rem"
+        }}
+      >
+        <Box display="flex" justifyContent={"center"} alignItems={"center"}>
+          {dropped.length === 0 ? <></> :
+            <>
+            {dropped[index] !== undefined ? <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>{dropped[index][0]}</div> : <></>}
+            {dropped[index] !== undefined ? <div style={{display:"flex", justifyContent:"center", alignItems:"center", marginLeft:"2rem"}}>{dropped[index][1]}</div> : <></>}
+            </>
+          }
+        </Box>
+      </div>
+    </Grid>
+  );
+};
+
 const DropTarget = (props:any) => {
   const isopenCode = props.openCode;
+  const rowCount = props.rowCount;
+  const columnCount = props.columnCount;
   const [sizewidth, setSizewidth] = useRecoilState(TemplateWidth);
   const [sizeheight, setSizeHeight] = useRecoilState(TemplateHeight);
   const [codestring, setCodestring] = useRecoilState(CodeString);
-  const [dropped, setDropped] = useState<React.ReactNode[] | any>([]);
-  const [dropped1, setDropped1] = useState<React.ReactNode[] | any>([]);
-  const [dropped2, setDropped2] = useState<React.ReactNode[] | any>([]);
-  const [dropped3, setDropped3] = useState<React.ReactNode[] | any>([]);
 
-  const [rowCount, setRowCount] = useState<number>(0);
-  const [columnCount, setColumnCount] = useState<number>(0);
+  const [dropped, setDropped] = useState<Array<React.ReactNode[] | any>>([]);
 
   useEffect(() => {
-    const columnCountMap: { [key: string]: number } = {
-      "400": 1,
-      "600": 2,
-      "800": 3,
-    };
-
-    setColumnCount(columnCountMap[sizewidth] || 0);
-  }, [sizewidth]);
+    if(rowCount !== 0 && columnCount !== 0){
+      setDropped(Array(rowCount * columnCount).fill([]));
+      setCodestring(Array(rowCount * columnCount).fill([]));
+    }
+  }, [rowCount, columnCount]);
 
   useEffect(() => {
-    const sizeToRowCountMap: { [key: string]: number } = {
-      "400": 10,
-      "600": 15,
-      "800": 20,
-    };
-
-    setRowCount(sizeToRowCountMap[sizeheight] || 0);
-  }, [sizeheight]);
-
-  useEffect(() => {
-    const Codetype = [...codestring];
-    Codetype[0] = dropped.length !== 0 ? getComponentType(dropped) : [];
-    setCodestring(Codetype);
+    function makeCodeString(){
+      const Codetype = dropped.map(drop => drop.length !== 0 ? getComponentType(drop) : []);
+      setCodestring(Codetype);
+    }
+    if(dropped.length !== 0){
+      makeCodeString();
+    }
   }, [dropped]);
-
-  const [{ canDrop: canDrop0, isOver: isOver0 }, drop0] = useDrop({
-    accept: [...DropAcceptList],
-    drop: (item, monitor) => {
-      if (monitor.isOver()) {
-        const Compo = [...dropped];
-        if (Compo.length === 2) {
-          alert('해당 영역에 더이상 컴포넌트를 추가할 수 없습니다.');
-        } else {
-          Compo.push(<DraggedComponent itemType={item} />);
-        }
-        console.log(Compo);
-        setDropped(Compo);
-      }
-    },
-    collect: (monitor) => ({
-      isOver: monitor.isOver(),
-      canDrop: monitor.canDrop(),
-    }),
-  });
-
-  const [{ canDrop: canDrop1, isOver: isOver1 }, drop1] = useDrop({
-    accept: [ItemTypes.BUTTON, ItemTypes.TEXTFIELD],
-    drop: (item, monitor) => {
-      if (monitor.isOver()) {
-        const Compo = [...dropped1];
-        if (Compo.length === 2) {
-          alert('해당 영역에 더이상 컴포넌트를 추가할 수 없습니다.');
-        } else {
-          Compo.push(<DraggedComponent itemType={item} />);
-        }
-        setDropped1(Compo);
-      }
-    },
-    collect: (monitor) => ({
-      isOver: monitor.isOver(),
-      canDrop: monitor.canDrop(),
-    }),
-  });
-
-  const [{ canDrop: canDrop2, isOver: isOver2 }, drop2] = useDrop({
-    accept: [ItemTypes.BUTTON, ItemTypes.TEXTFIELD],
-    drop: (item, monitor) => {
-      if (monitor.isOver()) {
-        const Compo = [...dropped2];
-        if (Compo.length === 2) {
-          alert('해당 영역에 더이상 컴포넌트를 추가할 수 없습니다.');
-        } else {
-          Compo.push(<DraggedComponent itemType={item} />);
-        }
-        setDropped2(Compo);
-      }
-    },
-    collect: (monitor) => ({
-      isOver: monitor.isOver(),
-      canDrop: monitor.canDrop(),
-    }),
-  });
-
-  const [{ canDrop: canDrop3, isOver: isOver3 }, drop3] = useDrop({
-    accept: [ItemTypes.BUTTON, ItemTypes.TEXTFIELD],
-    drop: (item, monitor) => {
-      if (monitor.isOver()) {
-        const Compo = [...dropped3];
-        if (Compo.length === 2) {
-          alert('해당 영역에 더이상 컴포넌트를 추가할 수 없습니다.');
-        } else {
-          Compo.push(<DraggedComponent itemType={item} />);
-        }
-        setDropped3(Compo);
-      }
-    },
-    collect: (monitor) => ({
-      isOver: monitor.isOver(),
-      canDrop: monitor.canDrop(),
-    }),
-  });
-
-  // 각 영역의 isActive 변수
-  const isActive0 = canDrop0 && isOver0;
-  const isActive1 = canDrop1 && isOver1;
-  const isActive2 = canDrop2 && isOver2;
-  const isActive3 = canDrop3 && isOver3;
-
-  function onClickClear(){
-    setDropped([]);
-    setDropped1([]);
-    setDropped2([]);
-    setDropped3([]);
-  }
   
+  const onClickClear = () => {
+    setDropped(Array(rowCount * columnCount).fill([]));
+  };
+
   function onClickBack(){
     setSizeHeight("");
     setSizewidth("");
   }
 
-
   return (
-    <React.Fragment>
+    <React.Fragment key={"d_d"}>
       {
         isopenCode === false ? (
-        <div>
+        <div key={"d_d_div"}>
           <Box display="flex" justifyContent={"space-between"} alignItems={"center"} sx={{mb:"0.5rem"}}>
             <IconButton sx={{float:"left", padding:"0.2rem", m:0, ...fontStyle}} onClick={onClickBack}><ArrowBackIcon/>Back</IconButton>
             <Typography variant='subtitle2'>{sizewidth} X {sizeheight}</Typography>
@@ -225,82 +190,12 @@ const DropTarget = (props:any) => {
           </Box>
           <Box sx={{width: `${sizewidth}px`,height: `${sizeheight}px`, border: '1px solid #000', p:"0.5rem"}}>
             <Grid container spacing={0} style={{height:"100%"}}>
-              <Grid item xs={12 / columnCount} style={{height: `${Math.floor(100 / rowCount)}%`}}>
-                <div
-                  key="0"
-                  id='drop0-0'
-                  ref={drop0}
-                  style={{
-                    width: `100%`,
-                    height: `100%`,
-                    border: dropped.length === 0 ? (isActive0 ? '1px dashed gray' : '1px solid gray') : "0",
-                    backgroundColor: isActive0 ? '#EEE' : 'transparent',
-                  }}
-                >
-                  {isActive0 ? '' : (dropped.length === 0 ? '여기로 아이템을 드래그하세요' : "")}
-                  <Box display="flex" justifyContent={"center"} alignItems={"center"} >
-                    <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>{dropped[0]}</div>
-                    <div style={{display:"flex", justifyContent:"center", alignItems:"center", marginLeft:"2rem"}}>{dropped[1]}</div>
-                  </Box>
-                </div>
-              </Grid>
-              <Grid item xs={12 / columnCount} style={{height: `${Math.floor(100 / rowCount)}%`}}>
-                <div
-                  key="1"
-                  id='drop0-1'
-                  ref={drop1}
-                  style={{
-                    width: `100%`,
-                    height: `100%`,
-                    border: isActive1 ? '1px dashed gray' : '0',
-                    backgroundColor: isActive1 ? '#EEE' : 'transparent',
-                  }}
-                >
-                  {isActive1 ? '' : (dropped1.length === 0 ? '여기로 아이템을 드래그하세요' : "")}
-                  <Box display="flex" justifyContent={"center"} alignItems={"center"} >
-                    <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>{dropped1[0]}</div>
-                    <div style={{display:"flex", justifyContent:"center", alignItems:"center", marginLeft:"2rem"}}>{dropped1[1]}</div>
-                  </Box>
-                </div>
-              </Grid>
-              <Grid item xs={12 / columnCount} style={{height: `${Math.floor(100 / rowCount)}%`}}>
-                <div
-                  key="2"
-                  id='drop1-0'
-                  ref={drop2}
-                  style={{
-                    width: `100%`,
-                    height: `100%`,
-                    border: isActive2 ? '1px dashed gray' : '0',
-                    backgroundColor: isActive2 ? '#EEE' : 'transparent',
-                  }}
-                >
-                  {isActive2 ? '' : (dropped2.length === 0 ? '여기로 아이템을 드래그하세요' : "")}
-                  <Box display="flex" justifyContent={"center"} alignItems={"center"} >
-                    <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>{dropped2[0]}</div>
-                    <div style={{display:"flex", justifyContent:"center", alignItems:"center", marginLeft:"2rem"}}>{dropped2[1]}</div>
-                  </Box>
-                </div>
-              </Grid>
-              <Grid item xs={12 / columnCount} style={{height: `${Math.floor(100 / rowCount)}%`}}>
-                <div
-                  key="3"
-                  id='drop1-1'
-                  ref={drop3}
-                  style={{
-                    width: `100%`,
-                    height: `100%`,
-                    border: isActive3 ? '1px dashed gray' : '0',
-                    backgroundColor: isActive3 ? '#EEE' : 'transparent',
-                  }}
-                >
-                  {isActive3 ? '' : (dropped3.length === 0 ? '여기로 아이템을 드래그하세요' : "")}
-                  <Box display="flex" justifyContent={"center"} alignItems={"center"} >
-                    <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>{dropped3[0]}</div>
-                    <div style={{display:"flex", justifyContent:"center", alignItems:"center", marginLeft:"2rem"}}>{dropped3[1]}</div>
-                  </Box>
-                </div>
-              </Grid>
+              {Array.from({ length: rowCount }).map((_, rowIndex) =>
+                Array.from({ length: columnCount }).map((_, columnIndex) => {
+                  const index = rowIndex * columnCount + columnIndex;
+                  return <DropArea key={index} index={index} dropped={dropped} setDropped={setDropped} columnCount={columnCount} rowCount={rowCount}/>;
+                })
+              )}
             </Grid>
           </Box>
         </div>
