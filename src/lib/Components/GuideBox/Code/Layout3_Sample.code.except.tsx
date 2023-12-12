c1b1a79e5460348e6c6f106fce14e2f2e19330a8
kh1012 <kh1012@midasit.com>
2023-12-12 13:31:38 +0900

    [StoryBook] Document 업데이트

--- src/lib/Components/GuideBox/Code/Layout3_Sample.code.except.tsx
+++ src/lib/Components/GuideBox/Code/Layout3_Sample.code.except.tsx
@@ -3,30 +3,21 @@ import { GuideBox } from "@midasit-dev/moaui";/**${comma}*/
 import {
 	TemplatesTendonProfileConverterUpdateButton,
 	TemplatesTendonProfileConverterList,
-	TemplatesTendonProfileConverterSelectButton,
-	TemplatesTendonProfileConverterHelpIconButton,
-	Typography, 
-	Button,
+	TemplatesTendonProfileConverterBottomButtons,
 } from "@midasit-dev/moaui";/**${comma}*/
 
 const ComponentsGuideBoxLayout3Sample = () => {
 	const visible = false;
 	return (
 		<GuideBox tag="Tendon Profile Converter" show={visible} fill='1' padding={1} itemSpacing={1}>
-			<GuideBox tag="Update Button" show={visible} fill='2' padding={1} itemCenter>
+			<GuideBox tag="Update Button" show={visible} fill='2'>
 				<TemplatesTendonProfileConverterUpdateButton />
 			</GuideBox>
-			<GuideBox tag="List" show={visible} fill='2' padding={1} itemCenter>
+			<GuideBox tag="List" show={visible} fill='2'>
 				<TemplatesTendonProfileConverterList />
 			</GuideBox>
-			<GuideBox tag="Select Button"	show={visible} fill='2' padding={1} itemCenter>
-				<TemplatesTendonProfileConverterSelectButton />
-			</GuideBox>
-			<GuideBox tag="Bottom Buttons" show={visible} fill='2' padding={1} itemDirection='row' itemCenter itemSpacing={3}>
-				<TemplatesTendonProfileConverterHelpIconButton />
-				<Typography flexItem textAlign='center'>Convert to</Typography>
-				<Button>New</Button>
-				<Button color="negative">Modify</Button>
+			<GuideBox tag="Bottom Buttons" show={visible} fill='2' itemSpacing={2.5} row>
+				<TemplatesTendonProfileConverterBottomButtons />
 			</GuideBox>
 		</GuideBox>
 	);
