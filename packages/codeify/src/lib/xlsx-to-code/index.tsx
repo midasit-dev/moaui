3206273382ea0957cf93fd5b40f344e23fd75951
kh1012 <kh1012@midasit.com>
2024-08-08 13:54:31 +0900

    update formula!

--- packages/codeify/src/lib/xlsx-to-code/index.tsx
+++ packages/codeify/src/lib/xlsx-to-code/index.tsx
@@ -1,16 +1,11 @@
 import React, { useCallback, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import * as ExcelJS from 'exceljs';
-import { Box, Container, Paper, Typography, Grid, Alert, Tabs, Tab, AlertTitle, Button } from '@mui/material';
+import { Box, Container, Paper, Typography, Grid, Alert, Tabs, Tab, AlertTitle, Button, TextField, Stack } from '@mui/material';
 import MonacoEditor from '@monaco-editor/react';
 import { toCssObjBorder, toCssObjFont, toCssObjFill } from './to-css';
 import { transform } from '@babel/standalone';
 import { Android12Switch } from './switch';
-
-interface CellWidthHeight {
-	width: number;
-	height: number;
-};
 
 function excelWidthToPixels(excelWidth: number): number {
 	// 일반적인 변환 공식
@@ -68,7 +63,6 @@ function toValue(value: ExcelJS.CellValue): string | number {
 	if (typeof value === 'object') {
 		//ExcelJS.CellFormulaValue
 		if ('formula' in value) {
-			console.log('formula', value.formula);
 		}
 
 		if ('result' in value) {
@@ -83,15 +77,20 @@ function toValue(value: ExcelJS.CellValue): string | number {
 	return 'toValue Error - Unsupported Type';
 }
 
+interface CellWidthHeight {
+	width: number;
+	height: number;
+}
+
 const ExcelToDiv: React.FC = () => {
-  const [excelData, setExcelData] = useState<ExcelJS.CellValue[][]>([]);
+  const [values, setValues] = useState<ExcelJS.CellValue[][]>([]);
   const [styles, setStyles] = useState<Partial<ExcelJS.Style>[][]>([]);
 	const [widthHeights, setWidthHeights] = useState<CellWidthHeight[][]>([]);
 	const [formulas, setFormulas] = useState<string[][]>([]);
   const [tabIndex, setTabIndex] = useState<number>(0);
 
   // Determine max number of columns to handle missing cells
-  const maxColumns = excelData.reduce((max, row) => Math.max(max, row.length), 0);
+  const maxColumns = values.reduce((max, row) => Math.max(max, row.length), 0);
 	const [previewByCode, setPreviewByCode] = useState<boolean>(false);
 	const [includedGridLines, setIncludedGridLines] = useState<boolean>(true);
 
@@ -115,14 +114,16 @@ const ExcelToDiv: React.FC = () => {
         await workbook.xlsx.load(arrayBuffer);
         const worksheet = workbook.worksheets[0];
 
-        const rawJson: ExcelJS.CellValue[][] = [];
+        const cellValues: ExcelJS.CellValue[][] = [];
         const cellStyles: Partial<ExcelJS.Style>[][] = [];
 				const cellWidthHeights: CellWidthHeight[][] = [];
+				const cellFormulas: string[][] = [];
 
         worksheet.eachRow({ includeEmpty: true }, (row: ExcelJS.Row, rowNumber: number) => {
-          const rowData: ExcelJS.CellValue[] = [];
+          const rowValues: ExcelJS.CellValue[] = [];
           const rowStyles: Partial<ExcelJS.Style>[] = [];
 					const rowWidthHeights: CellWidthHeight[] = [];
+					const rowFormulas: string[] = [];
 
 					const rowHeight = excelHeightToPixels(row.height ?? 17);
 
@@ -147,25 +148,27 @@ const ExcelToDiv: React.FC = () => {
 							value = '';
 						}
 
-            rowData[colNumber - 1] = value;
+            rowValues[colNumber - 1] = value;
 						rowWidthHeights[colNumber - 1] = { width: colWidth ?? 25, height: rowHeight ?? 25 };
 
 						// formula
-
+						if (cell.formula) {
+							console.log('cell.formula', cell.formula);
+							const formula = cell.formula;
+							rowFormulas[colNumber - 1] = formula;
+						}
           });
 
-          rawJson[rowNumber - 1] = rowData;
+          cellValues[rowNumber - 1] = rowValues;
           cellStyles[rowNumber - 1] = rowStyles;
 					cellWidthHeights[rowNumber - 1] = rowWidthHeights;
+					cellFormulas[rowNumber - 1] = rowFormulas;
         });
 
-				console.log(rawJson);
-				console.log(cellStyles);
-				console.log(cellWidthHeights);
-
-        setExcelData(rawJson);
+        setValues(cellValues);
         setStyles(cellStyles); // Save styles to state
 				setWidthHeights(cellWidthHeights);
+				setFormulas(cellFormulas);
       }
     };
 
@@ -178,6 +181,7 @@ const ExcelToDiv: React.FC = () => {
 			data: ExcelJS.CellValue[][], 
 			styles: Partial<ExcelJS.Style>[][], 
 			widthHeights: CellWidthHeight[][],
+			formulas: string[][]
 		): string => {
 			let codeString = `import React from 'react';\nconst App = () => {\n`;
 			codeString += `  return (\n`
@@ -219,6 +223,13 @@ const ExcelToDiv: React.FC = () => {
 					}
 
 					codeString += `          }}\n`;
+
+					// Add formula
+					const formula = formulas[rowIndex]?.[cellIndex];
+					if (formula) {
+						codeString += `          data-formula="${formula}"\n`;
+					}
+
 					codeString += `				 >\n`;
 
 					// *** BOX ABSOLUTE ***
@@ -258,6 +269,11 @@ const ExcelToDiv: React.FC = () => {
     setTabIndex(newValue);
   };
 
+	const [clickedCellInfo, setClickedCellInfo] = useState<{ 
+		rowIndex: number, 
+		cellIndex: number 
+	}>({ rowIndex: 0, cellIndex: 0 });
+
   return (
     <Container>
 
@@ -283,7 +299,7 @@ const ExcelToDiv: React.FC = () => {
         </Typography>
       </Paper>
       <Box>
-        {excelData.length > 0 ? (
+        {values.length > 0 ? (
 					<Box sx={{ display: "flex", justifyContent: "center", alignItems: 'center' }}>
 						<Tabs value={tabIndex} onChange={handleTabChange} centered>
 							<Tab label="Preview" />
@@ -313,54 +329,77 @@ const ExcelToDiv: React.FC = () => {
         )}
 
 				{/** Preview Tab */}
-        {excelData.length > 0 && tabIndex === 0 && (
-          <Grid container spacing={0} sx={{ marginTop: "20px" }}>
-            {excelData.length > 0 && tabIndex === 0 && (
-								<Grid container spacing={0} sx={{ marginTop: "20px" }}>
-									{excelData.map((row: ExcelJS.CellValue[], rowIndex: number) => (
-										<Grid item xs={12} key={rowIndex}>
-											<Box className="row" sx={{ display: "flex" }}>
-												{Array.from({ length: maxColumns }).map((_, cellIndex) => {
-													return (
+        {values.length > 0 && tabIndex === 0 && (
+          <Grid container spacing={0} sx={{ marginTop: "20px", }}>
+						<Stack direction='row' spacing={2} justifyItems='center'>
+							<TextField 
+								label='cell address'
+								variant='outlined'
+								size='small'
+								value={toCellAddress(clickedCellInfo.rowIndex, clickedCellInfo.cellIndex)}
+							/>
+							<TextField 
+								label='formula'
+								variant='outlined'
+								size='small'
+								value={formulas[clickedCellInfo.rowIndex]?.[clickedCellInfo.cellIndex] ?? '-'}
+							/>
+						</Stack>
+            {values.length > 0 && tabIndex === 0 && (
+							<Grid container spacing={0} sx={{ marginTop: "20px" }}>
+								{values.map((row: ExcelJS.CellValue[], rowIndex: number) => (
+									<Grid item xs={12} key={rowIndex}>
+										<Box className="row" sx={{ display: "flex" }}>
+											{Array.from({ length: maxColumns }).map((_, cellIndex) => {
+												return (
+													<Box
+														key={cellIndex}
+														sx={{
+															position: "relative",
+															width: widthHeights[rowIndex]?.[cellIndex]?.width ?? 25,
+															height: widthHeights[rowIndex]?.[cellIndex]?.height ?? 25,
+															overflow: "visible",
+															whiteSpace: "nowrap",
+															boxSizing: "border-box",
+															border: includedGridLines ? "1px solid rgba(0, 0, 0, 0.1)" : "none",
+															...toCssObjBorder(styles[rowIndex]?.[cellIndex]?.border),
+															...toCssObjFill(styles[rowIndex]?.[cellIndex]?.fill),
+															cursor: 'pointer',
+															'&:hover': {
+																border: '1px solid #217346',
+															},
+															'&:active': {
+																transform: 'scale(0.95)',
+															},
+															...(clickedCellInfo.rowIndex === rowIndex && clickedCellInfo.cellIndex === cellIndex ? { border: '1px solid #217346' } : {}),
+														}}
+														onClick={() => setClickedCellInfo({ rowIndex, cellIndex })}
+													>
 														<Box
-															key={cellIndex}
 															sx={{
-																position: "relative",
-																width: widthHeights[rowIndex]?.[cellIndex]?.width ?? 25,
-																height: widthHeights[rowIndex]?.[cellIndex]?.height ?? 25,
-																overflow: "visible",
-																whiteSpace: "nowrap",
-																boxSizing: "border-box",
-																border: includedGridLines ? "1px solid rgba(0, 0, 0, 0.1)" : "none",
-																...toCssObjBorder(styles[rowIndex]?.[cellIndex]?.border),
-																...toCssObjFill(styles[rowIndex]?.[cellIndex]?.fill),
+																position: "absolute",
+																top: 0,
+																left: 0,
+																zIndex: 1,
+																...toCssObjFont(styles[rowIndex]?.[cellIndex]?.font),
 															}}
 														>
-															<Box
-																sx={{
-																	position: "absolute",
-																	top: 0,
-																	left: 0,
-																	zIndex: 1,
-																	...toCssObjFont(styles[rowIndex]?.[cellIndex]?.font),
-																}}
-															>
-																{toValue(row[cellIndex])}
-															</Box>
+															{toValue(row[cellIndex])}
 														</Box>
-													)
-												})}
-											</Box>
-										</Grid>
-									))}
-								</Grid>
-							)}
+													</Box>
+												)
+											})}
+										</Box>
+									</Grid>
+								))}
+							</Grid>
+						)}
           </Grid>
         )}
 
 				{/** Code Tab */}
-        {excelData.length > 0 && tabIndex === 1 && (() => {
-					const tsxString = generateCode(excelData, styles, widthHeights);
+        {values.length > 0 && tabIndex === 1 && (() => {
+					const tsxString = generateCode(values, styles, widthHeights, formulas);
 					return (
 						<Box sx={{ height: "400px", marginTop: "20px" }}>
 							<Typography sx={{ mb: 2 }}>테스트 해보기 <a href='https://codesandbox.io/p/sandbox/xlsx-to-code-viewer-zzw54n?layout=%257B%2522sidebarPanel%2522%253A%2522EXPLORER%2522%252C%2522rootPanelGroup%2522%253A%257B%2522direction%2522%253A%2522horizontal%2522%252C%2522contentType%2522%253A%2522UNKNOWN%2522%252C%2522type%2522%253A%2522PANEL_GROUP%2522%252C%2522id%2522%253A%2522ROOT_LAYOUT%2522%252C%2522panels%2522%253A%255B%257B%2522type%2522%253A%2522PANEL_GROUP%2522%252C%2522contentType%2522%253A%2522UNKNOWN%2522%252C%2522direction%2522%253A%2522vertical%2522%252C%2522id%2522%253A%2522clzjfwql900063j6iobtkjc9p%2522%252C%2522sizes%2522%253A%255B100%252C0%255D%252C%2522panels%2522%253A%255B%257B%2522type%2522%253A%2522PANEL_GROUP%2522%252C%2522contentType%2522%253A%2522EDITOR%2522%252C%2522direction%2522%253A%2522horizontal%2522%252C%2522id%2522%253A%2522EDITOR%2522%252C%2522panels%2522%253A%255B%257B%2522type%2522%253A%2522PANEL%2522%252C%2522contentType%2522%253A%2522EDITOR%2522%252C%2522id%2522%253A%2522clzjfwql900023j6ifkhs5bn8%2522%257D%255D%257D%252C%257B%2522type%2522%253A%2522PANEL_GROUP%2522%252C%2522contentType%2522%253A%2522SHELLS%2522%252C%2522direction%2522%253A%2522horizontal%2522%252C%2522id%2522%253A%2522SHELLS%2522%252C%2522panels%2522%253A%255B%257B%2522type%2522%253A%2522PANEL%2522%252C%2522contentType%2522%253A%2522SHELLS%2522%252C%2522id%2522%253A%2522clzjfwql900033j6iczvgnt7h%2522%257D%255D%257D%255D%257D%252C%257B%2522type%2522%253A%2522PANEL_GROUP%2522%252C%2522contentType%2522%253A%2522DEVTOOLS%2522%252C%2522direction%2522%253A%2522vertical%2522%252C%2522id%2522%253A%2522DEVTOOLS%2522%252C%2522panels%2522%253A%255B%257B%2522type%2522%253A%2522PANEL%2522%252C%2522contentType%2522%253A%2522DEVTOOLS%2522%252C%2522id%2522%253A%2522clzjfwql900053j6idnzo4uo8%2522%257D%255D%257D%255D%252C%2522sizes%2522%253A%255B50%252C50%255D%257D%252C%2522tabbedPanels%2522%253A%257B%2522clzjfwql900023j6ifkhs5bn8%2522%253A%257B%2522tabs%2522%253A%255B%257B%2522id%2522%253A%2522clzjfwql800013j6isx52y8gf%2522%252C%2522mode%2522%253A%2522permanent%2522%252C%2522type%2522%253A%2522FILE%2522%252C%2522filepath%2522%253A%2522%252Fsrc%252Findex.tsx%2522%257D%255D%252C%2522id%2522%253A%2522clzjfwql900023j6ifkhs5bn8%2522%252C%2522activeTabId%2522%253A%2522clzjfwql800013j6isx52y8gf%2522%257D%252C%2522clzjfwql900053j6idnzo4uo8%2522%253A%257B%2522tabs%2522%253A%255B%257B%2522id%2522%253A%2522clzjfwql900043j6itdenx1e2%2522%252C%2522mode%2522%253A%2522permanent%2522%252C%2522type%2522%253A%2522UNASSIGNED_PORT%2522%252C%2522port%2522%253A0%257D%255D%252C%2522id%2522%253A%2522clzjfwql900053j6idnzo4uo8%2522%252C%2522activeTabId%2522%253A%2522clzjfwql900043j6itdenx1e2%2522%257D%252C%2522clzjfwql900033j6iczvgnt7h%2522%253A%257B%2522tabs%2522%253A%255B%255D%252C%2522id%2522%253A%2522clzjfwql900033j6iczvgnt7h%2522%257D%257D%252C%2522showDevtools%2522%253Atrue%252C%2522showShells%2522%253Atrue%252C%2522showSidebar%2522%253Atrue%252C%2522sidebarPanelSize%2522%253A15%257D' target='_blank'>Code Sandbox</a></Typography>
@@ -370,8 +409,8 @@ const ExcelToDiv: React.FC = () => {
 								language="typescript"
 								value={tsxString}
 								options={{
-										readOnly: true,
-										minimap: { enabled: false },
+									readOnly: false,
+									minimap: { enabled: false },
 								}}
 							/>
 							<Button onClick={() => setPreviewByCode(!previewByCode)} sx={{ mt: "10px" }}>Preview By Code Toggle</Button>
