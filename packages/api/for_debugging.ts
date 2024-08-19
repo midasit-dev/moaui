5351a79b51a5b0fd345ee4d708b5c4c42eebf847
kh1012 <kh1012@midasit.com>
2024-08-19 13:42:54 +0900

    update debugging code

--- packages/api/for_debugging.ts
+++ packages/api/for_debugging.ts
@@ -1,13 +1,28 @@
 import API from "@lib-apis";
 
-function main() {
+const getBaseUrl = () => "https://moa-engineers.midasit.com:443/civil";
+const getMapiKey = () =>
+  "eyJ1ciI6ImtoMTAxMkBtaWRhc2l0LmNvbSIsInBnIjoiY2l2aWwiLCJjbiI6InNUeFdRVjhyUVEifQ.1d5e81bb0f1bec0b53f76d870fdbb971c94601ed0c5f0e43fdb880d787d31383";
+
+async function main() {
   const api = new API({
-    productName: "CIVILNX",
-    mapiKey: "aaa",
-    baseUrl: "https://abc.com/civil",
+    baseUrl: getBaseUrl(),
+    mapiKey: getMapiKey(),
   });
-  const selfWeight = api.load.staticLoads.selfWeight;
-  console.log(selfWeight.get());
+  const rawNode = await api.db.nodeElement.node.getRaw();
+  console.log(rawNode["1"].X, rawNode["1"].Y, rawNode["1"].Z);
+
+  const helperNode = await api.db.nodeElement.node.get();
+  const Node1 = helperNode.getValue(1);
+  console.log(Node1.X, Node1.Y, Node1.Z);
+
+  const elems = await api.db.nodeElement.element.get();
+  elems.getValue(1).ANGLE;
+
+  const data2 = await api.combine.loadCombination.names.get({
+    includeEmpty: true,
+  });
+  console.log(data2);
 }
 
 main();
