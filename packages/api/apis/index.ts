240bba1bbfa0c8b41836b09d6d0685f00b6fb1c5
kh1012 <kh1012@midasit.com>
2024-08-19 13:42:46 +0900

    change initializer -> httpRequestInit

--- packages/api/apis/index.ts
+++ packages/api/apis/index.ts
@@ -1,18 +1,36 @@
-import Initializer, { InitProps } from "@lib-apis/declare/initializer";
-import Load from "@lib-apis/declare/civilnx/load";
+import DB from "@lib-apis/declare/civilnx/db";
+import Combine from "@lib-apis/declare/civilnx/combine";
+import HttpRequestInit, {
+  HttpRequestInitProps,
+} from "@lib-apis/declare/fetch/httpRequestInit";
 
+/**
+ * The main class that contains all the APIs.
+ * @example
+ * ```typescript
+ * import API from "@midasit-dev/moaui-apis";
+ * const api = new API({
+ *   baseUrl: getBaseUrl(),
+ *   mapiKey: getMapiKey(),
+ * });
+ *
+ * const nodes = await api.db.nodeElement.node.get();
+ * ...
+ * ```
+ */
 export default class API {
   // default informations
-  public init: Initializer = undefined;
+  public init: HttpRequestInit = undefined;
 
   // apis
-  public load: Load = undefined;
+  public db: DB = undefined;
+  public combine: Combine = undefined;
 
-  constructor(init: Initializer | InitProps) {
-    if (init instanceof Initializer) {
+  constructor(init: HttpRequestInit | HttpRequestInitProps) {
+    if (init instanceof HttpRequestInit) {
       this.init = init;
     } else if (init instanceof Object) {
-      this.init = new Initializer(init);
+      this.init = new HttpRequestInit(init);
     } else {
       throw new Error(
         "Please provide the initializer object or the instance of Initializer class."
@@ -20,6 +38,7 @@ export default class API {
     }
 
     //initialize apis!
-    this.load = new Load(init);
+    this.db = new DB(this.init);
+    this.combine = new Combine(this.init);
   }
 }
