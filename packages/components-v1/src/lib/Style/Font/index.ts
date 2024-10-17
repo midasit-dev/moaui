1dc854b7458f0be709f374fb644d37526110aa91
lhy0118 <lhy0118@midasit.com>
2024-10-17 14:15:57 +0900

    moaUI - Font에서 import하는 pretendard.css를 public폴더 내부 파일로 관리하도록 수정

--- packages/components-v1/src/lib/Style/Font/index.ts
+++ packages/components-v1/src/lib/Style/Font/index.ts
@@ -1,30 +1,34 @@
-import 'pretendard/dist/web/static/pretendard.css';
+import "/public/assets/fonts/pretendard.css";
 
 type FontSet = {
-	fontFamily: string;
-	letterSpacing: string;
-	fontFeatureSettings: string;
-}
+  fontFamily: string;
+  letterSpacing: string;
+  fontFeatureSettings: string;
+};
 class Font {
-	static readonly fontFamily = "Pretendard, sans-serif";
-	static readonly letterSpacing = "0rem"; /** 일괄적으로 변경하려면 수정 */
-	static readonly fontFeatureSettings = "'clig' off, 'liga' off";
-	static readonly defaultFontSet: FontSet = {
-		fontFamily: this.fontFamily,
-		letterSpacing: this.letterSpacing,
-		fontFeatureSettings: this.fontFeatureSettings,
-	}
+  static readonly fontFamily = "Pretendard, sans-serif";
+  static readonly letterSpacing = "0rem"; /** 일괄적으로 변경하려면 수정 */
+  static readonly fontFeatureSettings = "'clig' off, 'liga' off";
+  static readonly defaultFontSet: FontSet = {
+    fontFamily: this.fontFamily,
+    letterSpacing: this.letterSpacing,
+    fontFeatureSettings: this.fontFeatureSettings,
+  };
 
-	static getFontSize = (size: 'small' | 'medium' | 'large' | undefined) => {
-		if (!size) return '0.75rem'; /** 12px */
-		
-		switch (size) {
-			case 'small': return '0.75rem'; /** 12px */
-			case 'medium': return '1rem'; /** 16px */
-			case 'large': return '1.375rem' /** 22px */
-			default: return '0.75rem';
-		}
-	}
+  static getFontSize = (size: "small" | "medium" | "large" | undefined) => {
+    if (!size) return "0.75rem"; /** 12px */
+
+    switch (size) {
+      case "small":
+        return "0.75rem"; /** 12px */
+      case "medium":
+        return "1rem"; /** 16px */
+      case "large":
+        return "1.375rem"; /** 22px */
+      default:
+        return "0.75rem";
+    }
+  };
 }
 
 export default Font;
